3506dd0cce1d614cb3a54a23b1414c064c327658
Kókai Balázs <81510087+kokaibalazs@users.noreply.github.com>
2023-08-09 12:44:48 +0200

    Update and rename omni_a52xq.mk to twrp_a52xq.mk

--- twrp_a52xq.mk
+++ twrp_a52xq.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a52xq device
 $(call inherit-product, device/samsung/a52xq/device.mk)
 
 PRODUCT_DEVICE := a52xq
-PRODUCT_NAME := omni_a52xq
+PRODUCT_NAME := twrp_a52xq
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A526B
 PRODUCT_MANUFACTURER := samsung
